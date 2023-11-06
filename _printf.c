d06596d21370d23826a9ed014ef7f0db836548cc
Collinb190 <collinb190@gmail.com>
2023-11-06 12:33:37 -0600

    new code

---
+++ _printf.c
@@ -0,0 +1,59 @@
+#include "main.h"
+
+/*
+ * this code is for the team project
+ * the _printf function will handle the format specifiers c,s,%,d,i
+ */
+
+int _printf(const char *format, ...)
+{
+	/* Starts the count for each character printed and creats variable for iteration */
+	int count = 0;
+	int i = 0;
+
+	/* Initalizes the variable arguments list */
+	va_list args;
+	va_start (args, format);
+
+    while (format[i]) 
+    {
+        if (format[i] != '%') 
+        {
+            _putchar(format[i]);
+            count++;
+        } 
+        else 
+        {
+            i++;
+            if (format[i] == '%')
+            {
+                _putchar('%');
+                count++;
+            }
+            else if (format[i] == 'c') 
+            {
+                count += printChar(args);
+            } 
+            else if (format[i] == 's') 
+            {
+                count += printString(args);
+            } 
+            else if (format[i] == 'd' || format[i] == 'i') 
+            {
+                count += printNum(args);
+            } 
+            else 
+            {
+                _putchar('%');
+                _putchar(format[i]);
+                count += 2;
+            }
+        }
+        i++;
+    }
+
+    va_end(args);
+    return count;
+}
+
+	
--- main.h
+++ main.h
@@ -4,8 +4,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <stdarg.h>
 
 int _printf(const char *format, ...);
 int _putchar(char c);
+int printChar(va_list args);
+int printString(va_list args);
+int printNum(va_list args);
 
 #endif /*MAIN_H*/
