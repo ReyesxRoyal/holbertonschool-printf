3f79c0c92951d83f0c93a73dbbede3ca4aa74b5c
Collinb190 <collinb190@gmail.com>
2023-11-06 22:57:38 -0600

    betty fix

--- myFuns.c
+++ myFuns.c
@@ -4,91 +4,35 @@
  * printChar - handles the %c
  * @args: character that is being counted
  *
- * Description: this will conatin a detailed description.
- * Return: number of chars counted
-*/
+ * Description: Prints a character specified by the %c format.
+ * Return: The number of characters printed.
+ */
 int printChar(va_list args)
 {
 	int c = va_arg(args, int);
-
 	return (_putchar(c));
 }
+
 /**
  * printString - handles the %s
  * @args: string that is being counted
  * 
- * Description: this will conatin a detailed description.
- * Return: number of chars counted
-*/
+ * Description: Prints a string specified by the %s format.
+ * Return: The number of characters printed.
+ */
 int printString(va_list args) 
 {
 	int i = 0;
-
 	char *str = va_arg(args, char *);
 
 	if (str == NULL)
-		{
-			str = "(null)";
-		}
+	{
+		str = "(null)";
+	}
 	while (str[i] != '\0')
-		{
-			_putchar(str[i]);
-			i++;
-		}
+	{
+		_putchar(str[i]);
+		i++;
+	}
 	return (i);
-}
-/**
- * printNum - handles the %d & %i
- * @args: number that is being counted
- * 
- * Description: this will conatin a detailed description.
- * Return: number of chars counted
-*/
-int printNum(va_list args) 
-{
-	int num = va_arg(args, int);
-	int count = 0;
-	int digits;
-	int temp;
-	int divisor;
-	int dig;
-	int i;
-
-	if (num < 0) /* this handles negative numbers */
-	{
-		_putchar('-');
-		count++;
-		num = -num;
-	}
-
-	if (num == 0) /* this handles 0 */
-	{
-		_putchar('0');
-		return (1);
-	}
-	else
-	{
-		digits = 0;
-		temp = num;
-		while (temp != 0)
-		{
-			temp /= 10;
-			digits++;
-		}
-
-		divisor = 1;
-		for (i = 1; i < digits; i++)
-		{
-			divisor *= 10;
-		}
-		while (divisor > 0)
-		{
-			dig = num / divisor;
-			_putchar(dig + '0');
-			count++;
-			num %= divisor;
-			divisor /= 10;
-		}
-	}
-	return (count);
 }
